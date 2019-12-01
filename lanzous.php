ff70a76c4ba1bc2b20dd500db1d4aa34fb7f3521
fubumiodd <45312898+fubumiodd@users.noreply.github.com>
2019-12-01 22:45:07 +0800

    Update lanzous.php

--- lanzous.php
+++ lanzous.php
@@ -1,68 +1,49 @@
 <?php
-/**
- * @package Lanzou
- * @author Mlooc
- * @version 1.0.0
- * @link https://mlooc.cn
- */
-// 指定允许其他域名访问  
-header('Access-Control-Allow-Origin:*');
-    function MloocCurl($url,$method,$ifurl,$post_data){
-        $UserAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36';#设置ua
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_USERAGENT, $UserAgent);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        if ($method == "post") {
-            curl_setopt($curl, CURLOPT_REFERER, $ifurl); 
-            curl_setopt($curl, CURLOPT_POST, 1);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, $post_data);
-        }
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return $response;
-    }
-    if (!empty($_GET['url'])) {
-        $url = $_GET['url'];
-        #第一步
-        $ruleMatchDetailInList = "~ifr2\"\sname=\"[\s\S]*?\"\ssrc=\"\/(.*?)\"~";
-        preg_match($ruleMatchDetailInList, MloocCurl($url,null,null,null),$link);
-        $ifurl = "https://www.lanzous.com/".$link[1];
-        #第二步
-        $ruleMatchDetailInList = "~=\s'(.*?)';[\S\s]*?=\s'(.*?)'[\S\s]*?=\s'(.*?)'[\S\s]*?=\s'(.*?)'~";
-        preg_match($ruleMatchDetailInList, MloocCurl($ifurl,null,null,null),$segment);
-        #第三步
-        #post提交的数据
-        $post_data = array(
-            "action" => $segment[1],
-            "file_id" => $segment[2],
-            "t" => $segment[3],
-            "k" => $segment[4]
-            );
-        $obj = json_decode(MloocCurl("https://www.lanzous.com/ajaxm.php","post",$ifurl,$post_data));#json解析
-        if ($obj->dom == "") {#判断链接是否正确
-            echo "链接有误！";
-        }else{
-            $downUrl = $obj->dom."/file/".$obj->url;
-            if (!empty($_GET['type'])) {
-                $type = $_GET['type'];
-                if ($type == "down") {
-                    header('Location:'.$downUrl);#直接下载
-                }else{
-                    echo $obj->dom."/file/".$obj->url;#输出直链
-                }
-            }else{
-                echo $obj->dom."/file/".$obj->url;#输出直链
-            }
-        }
-    }else{
-        $result_url = str_replace("lanzous.php","","//".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']."?url=https://www.lanzous.com/i19zisb");
-        echo "示列：";
-        echo "<br/>";
-        echo "直接下载："."<a href='".$result_url."&type=down' target='_blank'>".$result_url."&type=down</a>";
-        echo "<br/>";
-        echo "输出直链："."<a href='".$result_url."' target='_blank'>".$result_url."</a>";
-    }
+#coding=utf-8
+from bs4 import BeautifulSoup
+import requests
+import re
+import json
+ 
+#蓝奏云分享文件链接地址
+url = 'https://www.lanzous.com/i3xcmaf'
+ 
+#header头，注意那个referer必须要与上面文件分享地址url相同
+headers = {
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.3',
+    'referer': url
+}
+ 
+# 获取分享页面html文件
+res = requests.get(url,headers=headers)
+ 
+# 引入BeautifulSoup库对html进行处理，获取iframe中的出现的js文件
+soup = BeautifulSoup(res.text,'html.parser')
+url2 = 'https://www.lanzous.com/'+soup.find('iframe')['src']
+res2 = requests.get(url2,headers=headers)
+ 
+# 正则提取请求三个参数
+a = re.findall(r'var a = \'([\w]+?)\';',res2.text)
+params = re.findall(r'var [\w]{6} = \'([\w]+?)\';',res2.text)
+ 
+# 请求下载地址
+url3 = 'https://www.lanzous.com/ajaxm.php'
+data = {
+    'action':'down_process',
+    'file_id':params[0],
+    't':params[1],
+    'k':params[2],
+}
+res3 = requests.post(url3,headers=headers,data=data)
+res3 = json.loads(res3.content)
+ 
+# 请求最终重定向地址
+url4 = res3['dom']+'/file/'+res3['url']
+headers2 = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0',
+    'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+}
+res4 = requests.head(url4, headers=headers2)
+print res4.headers['Location']
+
 ?>
